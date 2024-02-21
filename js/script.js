4fe19677c79bfe28bb0bfe15ed5920b28f332127
Luca <lucafagiolini@live.it>
2024-02-21 18:13:02 +0100

    bonus completed

--- js/script.js
+++ js/script.js
@@ -5,22 +5,24 @@ createApp({
         return {
             // tutti i nostri dati
             emailList: [],
+
+            emailCounter: 0,
         }
     },
 
     mounted() {
 
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 30; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
 
                 this.emailList.push(response.data.response);
+                this.emailCounter++;
+                console.log(this.emailCounter);
             });
         }
         console.log(this.emailList);
 
-
-
-
     },
+
 }).mount("#app");
 // ******** VUE ********
