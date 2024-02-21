b14dfc28c2f5ca38e039053a3f8fd1077cf76c9e
Luca <lucafagiolini@live.it>
2024-02-21 16:54:12 +0100

    milestone-1

--- js/script.js
+++ js/script.js
@@ -10,10 +10,15 @@ createApp({
 
     mounted() {
 
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-            this.emailList = response.data.response;
-            console.log(this.emailList);
-        });
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+
+                this.emailList.push(response.data.response);
+            });
+        }
+        console.log(this.emailList);
+
+
 
 
     },
